33d03e6180acbc0ae160bba8bb1f305d686fc1bb
Mauricio Apaza Carpio <mpacarpio@gmail.com>
2022-07-26 18:56:37 -0500

    Add Logo, Maquetacion de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,25 @@ import { Route } from "wouter";
 import Result from "./Pages/Result/index";
 import Navbar from "./Components/Simple/Navbar/Navbar";
 import Search from "./Pages/Search/index";
+import { GifContextProvider } from "./Context/GifContext";
+import Details from "./Pages/Details";
+import Buscador from "./Components/Simple/Buscador/Buscador";
+
 function App() {
   return (
-    <section className="App-header">
+    <section className="App">
       <Navbar></Navbar>
-      <Route path="/" component={Home} />
-      <Route path="/Result/:keyword" component={Result} />
-      <Route path="/Search" component={Search} />
+      <div className="App-buscador">
+        <Buscador></Buscador>
+      </div>
+      <div className="App-body">
+        <GifContextProvider>
+          <Route path="/" component={Home} />
+          <Route path="/Result/:keyword" component={Result} />
+          <Route path="/Search" component={Search} />
+          <Route path="/Details/:id" component={Details} />
+        </GifContextProvider>
+      </div>
     </section>
   );
 }
