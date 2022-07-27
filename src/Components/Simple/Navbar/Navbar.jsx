751419c35c2b19ec3e32c3156274fa34dbe527fe
Mauricio Apaza Carpio <mpacarpio@gmail.com>
2022-07-26 19:01:17 -0500

    add logo al componente navbar

--- src/Components/Simple/Navbar/Navbar.jsx
+++ src/Components/Simple/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "wouter";
+import logo from "../../../Assets/Logo-w.svg";
 import "./Navbar.css";
+
 export default function Navbar() {
   return (
     <>
       <nav className="Navbar">
         <Link to="/">
-          <a href="">Logooo</a>
+          <a href="" className="Navbar-logo">
+            <img src={logo} alt="" srcset="" /> Giffy
+          </a>
         </Link>
         <ul>
           <Link to="/Search ">
